8084ce72959fe689a90f9f30662c405952336556
stephane.malho <stepahen.malho@gmail.com>
2023-08-26 08:47:46 +0200

    ✨ F01: HOME > add handleTaskCreate function

--- todo-app/src/api/tasksAPI.js
+++ todo-app/src/api/tasksAPI.js
@@ -1,12 +1,21 @@
+import axios from "axios";
 import { API_BASE_URL } from "./baseURL";
+import { options } from "./options";
 
 export const getTasks = async () => {
-  const response = await fetch(`${API_BASE_URL}/tasks`);
-  return await response.json();
+  return axios
+    .get(`${API_BASE_URL}/tasks`)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
 };
 
-export const createTask = async (label) => {
-  const response = await fetch(`${API_BASE_URL}/tasks/${label}`);
-  return await response.json();
-}
-
+export const createTask = async (newTaskData) => {
+  return axios
+    .post(`${API_BASE_URL}/tasks`, newTaskData, options)
+    .then((response) => response.data)
+    .catch((error) => {
+      throw error;
+    });
+};
--- todo-app/src/components/pages/Home.jsx
+++ todo-app/src/components/pages/Home.jsx
@@ -1,37 +1,79 @@
 import { useEffect, useState } from "react";
-import { getTasks } from "../../api/tasksAPI";
+import { createTask, getTasks } from "../../api/tasksAPI";
 
 const Home = () => {
   // STATE
-  
-  const [tasks, setTasks] = useState({})
+  const [tasks, setTasks] = useState([]);
   console.log(tasks);
-  const [newTask, setNewTask] = useState('')
+  const [newTask, setNewTask] = useState("");
+  console.log(newTask);
+  const [description, setDescription] = useState("");
 
   // BEHAVIOR
   const fetchTasks = async () => {
     const data = await getTasks();
-    setTasks(data)
-  }
+    setTasks(data);
+  };
+
+  const handleTaskCreate = async () => {
+    try {
+      const newTaskData = {
+        label: newTask,
+        description: description,
+        start_date: new Date().toISOString(),
+        end_date: "",
+      };
+      const createdTask = await createTask(newTaskData);
+      setTasks((prevTasks) => [...prevTasks, createdTask]); // Utilisation du callback
+      setNewTask("");
+      setDescription("");
+    } catch (error) {
+      console.error("Erreur lors de la création de la tâche :", error);
+    }
+  };
+  
 
   useEffect(() => {
     fetchTasks();
-  }, [])
+  }, [newTask]);
 
   const handleChange = (e) => {
-    setNewTask(e.target.value)
-  }
-  
+    setNewTask(e.target.value);
+  };
+  const handleChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
   // JSX
   return (
     <div className="todo-container">
       <h1>Home</h1>
-      <p>
-        {tasks?.[0]?.label}
-      </p>
-      <input id="new-task" type="text" value={newTask} onChange={handleChange} />
+      <p>{tasks?.[0]?.label}</p>
+      <from>
+        <label htmlFor="new-task">New task</label>
+        <input
+          id="new-task"
+          type="text"
+          value={newTask}
+          onChange={handleChange}
+        />
+        <label htmlFor="new-description">New Description</label>
+        <input
+          id="new-description"
+          type="text"
+          value={description}
+          onChange={handleChangeDescription}
+        />
+        <button onClick={handleTaskCreate}>Add task</button>
+      </from>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <p>{task.label}</p>
+          <p>{task.description}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
